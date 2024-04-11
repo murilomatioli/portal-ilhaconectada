54bdabf36dee5845d94e4d22cc2e76a9da9e4ece
Matiolilegalzao <murilomatioli15@gmail.com>
2024-04-11 07:23:48 -0300

    atualização

--- src/components/footerComponent.jsx
+++ src/components/footerComponent.jsx
@@ -3,15 +3,26 @@ import styles from "./footer.module.css";
 function FooterComponent() {
     return(
         <>
-        
         <div className={styles.footerBody}>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+            <div className={styles.footerWrapper}>
+            <div className={styles.contact}>
+                <h2>Contato</h2>
+                <ul>
+                    <li>Telefone: 18 23456-7890</li>
+                    <li>Email: informatica@ilhasolteira.br</li>
+                    <li>Endereço: Praça Paiáguas, Centro</li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div>
+                <h2>Funcionamento</h2>
+                <ul>
+                    <li>Segunda a sexta - 7:30 às 17:00</li>
+                </ul>
+                <p></p>
+            </div>
+            </div>
         </div>
         </>
     )
