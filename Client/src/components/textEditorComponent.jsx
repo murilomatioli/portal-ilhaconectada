281a9e224b5f5539b307f225b36ab8f6b7b3d29e
Matiolilegalzao <murilomatioli15@gmail.com>
2024-04-17 00:35:43 -0300

    Publicar artigo funcionando

--- Client/src/components/textEditorComponent.jsx
+++ Client/src/components/textEditorComponent.jsx
@@ -4,11 +4,24 @@ import { useEffect } from "react";
 import axios from 'axios';
 function TextEditorComponent() {
   const [content, setContent] = useState("");
+
+  const collectData = async (e) => {
+    e.preventDefault();
+    let result = await fetch('http://localhost:4000/', {
+      method: 'post',
+      body: JSON.stringify({content}),
+      headers:{
+        'Content-Type': 'application/json'
+      },
+    });
+    result = await result.json
+    localStorage.setItem("content", JSON.stringify(result));
+  }
   return (
     <>
-      <form>
+      <form onSubmit={collectData}>
         <h1 className={styles.title}>&gt; Publicar novo artigo</h1>
-        <textarea className={styles.textArea} placeholder="Escreva suas ideias..."></textarea>
+        <textarea className={styles.textArea} value={content} onChange={(e) => setContent(e.target.value)} placeholder="Escreva suas ideias..."></textarea>
         <button type="submit" title="Publicar conteúdo" className={styles.publicarBtn}>Publicar</button>
       </form>
     </>
--- Server/Index.js
+++ Server/Index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
+const cors = require('cors')
+app.use(cors());
+
 require('./db/connection');
 const Post = require('./Models/Post')
 app.post("/", async(req,res)=> {
